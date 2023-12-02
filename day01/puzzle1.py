b824715efaa121e8396bb5a1e785b817aeeb2987
gabriela <sg.villegasq@gmail.com>
2023-12-02 19:16:08 +0000

    added solution for day1, puzzle1

---
+++ day01/puzzle1.py
@@ -0,0 +1,22 @@
+def calibration(filename: str) -> int:
+    total = 0
+    with open(filename, "r") as f:
+        for line in f:
+            line = line.strip()
+            for i in range(len(line)):
+                if line[i].isdigit():
+                    left = line[i]
+                    break
+            for j in range(len(line) - 1, -1, -1):
+                if line[j].isdigit():
+                    right = line[j]
+                    break
+
+            total += int(left + right)
+
+    return total
+
+
+assert calibration("day01/puzzle1_test_input") == 142
+
+print(calibration("day01/puzzle1_input"))
